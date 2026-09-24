783e6027ed9ca2af15da3f33203164b4d5836902
kaydence-c. <kaydence.clark23@gmail.com>
2026-09-23 22:49:32 -0500

    Final product

--- Treact Starter Resources (1) 2/script.js
+++ Treact Starter Resources (1) 2/script.js
@@ -1,7 +1,13 @@
 function openMenu() {
     document.body.classList.add("menu--open");
+    document.querySelector(".menu").setAttribute("aria-hidden", "false");
 }
 
 function closeMenu() {
     document.body.classList.remove("menu--open");
+    document.querySelector(".menu").setAttribute("aria-hidden", "true");
 }
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") closeMenu();
+});
